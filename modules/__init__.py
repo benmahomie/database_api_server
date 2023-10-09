cbeb5eb8beba49ce35060e70b9344c3d37ac8412
Ben <109660600+benmahomie@users.noreply.github.com>
2023-10-09 13:50:33 -0600

    working modularized

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,17 +2,27 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, ALL
 import logging
+import secrets
 
+# Signature configuration
+secret = 'secret' # Uncomment for testing (not secure)
+# secret = secrets.token_hex(16) # Uncomment for deployment (uses HMAC hash like SHA-256)
+
+# Start Flask App
 app = Flask(__name__)
-app.secret_key = 'secret'
+app.secret_key = secret 
 
+# Login Settings
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+# Database Configuration (points to the folder holding database)
 files = UploadSet('files', ALL)
-app.config['UPLOADED_FILES_DEST'] = 'file_storage'
+app.config['UPLOADED_FILES_DEST'] = 'file_storage' 
 configure_uploads(app, files)
 
-logging.basicConfig(level=logging.DEBUG)
+# Logging Configuration (for hard-to-catch issues)
+logging.basicConfig(level=logging.DEBUG) 
 
+# App Routes
 from . import routes
--- modules/models.py
+++ modules/models.py
@@ -1,13 +1,20 @@
 from flask_login import UserMixin
+from . import login_manager
 
+# User Object Class for Flask-Login
 class User(UserMixin):
     def __init__(self, id, permission='read', password=None):
         self.id = id
         self.password = password
         self.permission = permission
 
+# User Configuration (ID, Permission Level, Password)
 users = {
-    'admin': User('admin', 'edit', 'admin'),
-    'user': User('user', 'download', 'user'),
-    'guest': User('guest', 'read', 'guest')
+    'admin': User(id='admin', permission='edit', password='admin'),
+    'user': User(id='user', permission='download', password='user'),
+    'guest': User(id='guest', permission='read', password='guest')
 }
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.get(user_id)
